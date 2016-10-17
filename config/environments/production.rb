9076e550a1f20cabb9024dc8a59b1a54489baa26
Gledson Cruz <gledson.cruz@gmail.com>
2016-10-17 14:46:15 -0200

    Corrige dominio email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN_SITE'] }
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
 end
